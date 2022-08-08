ab3ad90b762f5b038785df6d4f85ae3734472dc4
Paulina Ramirez <paulina.rn@gmail.com>
2022-08-08 15:50:23 -0500

    Write function that returns its input reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const newStr = str.split("");
+    const reverseArray = newStr.reverse();
+    const joinArray = reverseArray.join("")
+    return (joinArray);
 
+       /* newStr.join("");
+    
+    const length = str.length;
+    
+    reverseStr = newStr[4] + newStr[3] + newStr[2];
+    return reverseStr */
 };
 
 // Do not edit below this line
