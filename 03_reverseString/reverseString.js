56ea29303b502255157fbccd30d86db89ac93f73
Paulina Ramirez <paulina.rn@gmail.com>
2022-08-08 15:51:17 -0500

    Correct function that reverses input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,13 +3,6 @@ const reverseString = function(str) {
     const reverseArray = newStr.reverse();
     const joinArray = reverseArray.join("")
     return (joinArray);
-
-       /* newStr.join("");
-    
-    const length = str.length;
-    
-    reverseStr = newStr[4] + newStr[3] + newStr[2];
-    return reverseStr */
 };
 
 // Do not edit below this line
