2a984237142ee4d979f324e265e4e264e1e59db5
Paulina Ramirez <paulina.rn@gmail.com>
2022-08-08 16:04:53 -0500

    Splice array to remove given argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, rem) {
+    for(let i = 0; i < arr.length; i++){
+        if ( arr[i] == rem) { 
+            return (arr.splice(i, 1)); 
+        }
+    }
 };
 
 // Do not edit below this line
