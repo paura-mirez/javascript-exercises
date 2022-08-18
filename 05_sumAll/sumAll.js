bc368496739b8efe00de28a368bee4f927731c8c
Paulina Ramirez <paulina.rn@gmail.com>
2022-08-18 16:57:14 -0500

    Implement function that sums 2 integers and the numbers between them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let finalSum = 0
+    
+    if ( typeof num1==='number' && typeof num2==='number' && num1>=0 && num2>=0) {
+        if (num1<num2) { 
+            let i=num1
+            while (i<=num2)
+            {
+                finalSum = finalSum + i;
+                i++;
+            }
+        }
+        else {
+            let i=num2
+            while (i<=num1)
+            {
+                finalSum = finalSum + i;
+                i++;
+            }
+        }
+        
+    
+        return finalSum;
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
