a955becbd1f3b216eeb6010b90cc081d80544052
Paulina Ramirez <paulina.rn@gmail.com>
2022-08-18 21:00:35 -0500

    Create function that determines if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let div4 = year % 4
+    let div100 = year % 100
+    let div400 = year % 400
+
+    if ((div4 == 0 && div100 !== 0) == false) {
+        return (div400==0)
+    }
+    else {
+        return (div4 == 0 && div100 !== 0)
+    }
 
 };
 
