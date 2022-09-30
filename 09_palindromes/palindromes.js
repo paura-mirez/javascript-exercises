e7060d9c5c542c562aacd358e8f2340d2dff08e5
Paulina Ramirez <paulina.rn@gmail.com>
2022-09-30 14:41:40 -0500

    Write function to detect palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let onlyLetters = text.toLowerCase().replace(/[^a-zA-Z ]/g, "");
+    let original = onlyLetters.split(' ').filter(char => /[a-z]/.test(char)).join('').split('');
+    reversed = [...original].reverse(); 
+    console.log (original, reversed);
+    
+    if (original === reversed) return true;
+    if (original == null || reversed == null) return false;
+    if (original.length !== reversed.length) return false;
 
+    for (let i = 0; i < original.length; ++i) {
+        if (original[i] !== reversed[i]) return false;
+      }
+    return true;
 };
 
 // Do not edit below this line
