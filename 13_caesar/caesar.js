3f5f68fbe2d6b8358b8be0e2fded9eaeb3b127db
Paulina Ramirez <paulina.rn@gmail.com>
2022-10-02 15:08:16 -0500

    Create function to implement Caesar cipher

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,13 @@
-const caesar = function() {
-
+const caesar = function(word, num) {
+    array=Array.from(word);
+    let newString = [];
+    array.forEach(letter => {
+        let unicode= letter.charCodeAt(0);
+        let newUnicode = unicode + num;
+        let newLetter = String.fromCharCode(newUnicode);
+        newString.push(newLetter);
+    });
+   return (newString.join(''));
 };
 
 // Do not edit below this line
