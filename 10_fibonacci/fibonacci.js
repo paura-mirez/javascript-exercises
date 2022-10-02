8e11b8789326374e6057aa895f2f6d5ad38d2f03
Paulina Ramirez <paulina.rn@gmail.com>
2022-10-02 09:47:22 -0500

    Create function to return specific number of fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num=(num*1);
+    if (num<0) {
+        return "OOPS"
+    }
+    else {
+        let fibSequence = [0,1,];
+        let numberOne=0;
+        let numberTwo=1;
+        let newNumber= numberOne + numberTwo;
+        fibSequence.push(newNumber);
+        
+        for (let i=1; i < num ; i++) {
+            numberOne=numberTwo;
+            numberTwo=newNumber;
+            newNumber= numberOne + numberTwo;
+            fibSequence.push(newNumber);
+        }
+        
+        console.log(fibSequence + "this is numberOne now " + numberOne + " and this is numberTwo " + numberTwo);
+        return fibSequence[num];
+    }
+    
 };
 
 // Do not edit below this line
