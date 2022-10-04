7f510917246a47833856ad0dbc1684ca85311e31
Paulina Ramirez <paulina.rn@gmail.com>
2022-10-03 21:50:12 -0500

    Fix function parameter to take arrays

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,22 +1,4 @@
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
-
-const findTheOldest = function() {
+const findTheOldest = function(people) {
     people.forEach(person => {
         if (person.yearOfDeath != null) {
             let age = person.yearOfDeath - person.yearOfBirth;
