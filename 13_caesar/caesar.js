46d79daeb9f99d6862145f1d3cdf41e432ead2af
Paulina Ramirez <paulina.rn@gmail.com>
2022-10-08 17:24:22 -0500

    Add modulus operator to account for large shifts

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,13 +1,45 @@
-const caesar = function(word, num) {
-    array=Array.from(word);
+const caesar = function(text, shift) {
+    
+    //create array from string
+    array=Array.from(text);
+    
+    //declare newString variable where the new letters will be pushed to
     let newString = [];
+    
+    //for each letter of the array, do the following
     array.forEach(letter => {
+        //change each letter to unicode
         let unicode= letter.charCodeAt(0);
-        let newUnicode = unicode + num;
-        let newLetter = String.fromCharCode(newUnicode);
-        newString.push(newLetter);
+        if (shift < 0) { shift = 26 + (shift % 26);}
+        console.log(shift);
+        
+        //if the unicode corresponds to A-Z then add the number to create a new unicode
+        if (unicode> 64 && unicode < 91) {
+            console.log(shift);
+            let newUnicode = (unicode + (shift-65)) % 26 + 65;
+            console.log(newUnicode);
+            let newLetter = String.fromCharCode(newUnicode);
+            newString.push(newLetter);
+            }
+        
+        //if the unicode corresponds to a-z then add the number to create a new unicode
+        else if (unicode> 96 && unicode< 123) {
+            let newUnicode = (unicode + (shift-97)) % 26 + 97;
+            let newLetter = String.fromCharCode(newUnicode);
+            newString.push(newLetter);
+            }
+        
+        // if unicode does not correspond to A-Z or a-z then 
+        //turn back into string and push it to new string
+        else {
+            let oldLetter = String.fromCharCode(unicode);
+            newString.push(oldLetter);
+        }
+
     });
-   return (newString.join(''));
+   
+   //turn the new string array into a string and return that
+    return (newString.join(''));
 };
 
 // Do not edit below this line
